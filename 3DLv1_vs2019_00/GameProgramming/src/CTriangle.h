0a0351af596bbde9f57aaffe686002ddf0d422cb
Ohkawa-atsuya-Fukuyama <gc23008@anabuki-net.ne.jp>
2023-09-27 10:53:59 +0900

    03

---
+++ 3DLv1_vs2019_00/GameProgramming/src/CTriangle.h
@@ -0,0 +1,16 @@
+#ifndef CTRIANGLE_H
+#define CTRIANGLE_H
+
+#include "CVector.h"
+
+class CTriangle
+{
+public:
+	void Vertex(const CVector& v0, const CVector& v1, const CVector& v2);
+	void Normal(const CVector& n);
+	void Render();
+private:
+	CVector mV[3];
+	CVector mN;
+};
+#endif
